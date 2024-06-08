e847fd3a8903668295a925a67014f24b38dcb205
PC <PC@DESKTOP-2S86EDB.home>
2024-06-08 10:17:43 +0100

    Primeiro Commit

---
+++ LupeInfinito/src/lupeinfinito/LupeInfinito.java
@@ -0,0 +1,15 @@
+
+package lupeinfinito;
+
+
+public class LupeInfinito {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        
+        while(true){
+        System.out.println(++contador);
+        }
+    }
+    
+}
